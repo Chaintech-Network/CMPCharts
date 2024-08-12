b75a7bc5a6cba6abf7abcde72bd62ce88285aa60
ctn-mobile-innovation <rehan.t@chaintech.network>
2024-08-12 06:06:42 +0000

    CMPCharts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -54,7 +54,7 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation("network.chaintech:cmpcharts:1.0.0")
+            implementation("network.chaintech:cmpcharts:1.0.2")
         }
 
         commonTest.dependencies {
